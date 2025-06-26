025bc29715b70235681fc7cb5944e6e93497b5f5
lyonHunchoNganga <lyonnganga1@gmail.com>
2025-06-27 01:18:18 +0300

    Remove commented section for To-Do List in index.js

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ function showRetryButton() {
   }
 }
 
-// === To-Do List ===
+
 const todoForm = document.getElementById("todo-form");
 const todoInput = document.getElementById("todo-input");
 const todoList = document.getElementById("todo-list");
