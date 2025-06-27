ad52ea19c8da39fefa4127293dafefa951b06947
lyonHunchoNganga <lyonnganga1@gmail.com>
2025-06-27 11:55:51 +0300

    Add EventCounts placeholder at the end of index.js

--- index.js
+++ index.js
@@ -113,3 +113,4 @@ const toggle = document.getElementById("dark-mode-toggle");
 toggle.addEventListener("change", () => {
   document.body.classList.toggle("dark-mode", toggle.checked);
 });
+EventCounts
